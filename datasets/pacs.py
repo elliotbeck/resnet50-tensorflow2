f85bbf69dabd5792f3a3e6f306d4aa81e83002af
elliotbeck <elliot.beck@hotmail.com>
2019-11-25 17:21:37 +0100

    test

--- datasets/pacs.py
+++ datasets/pacs.py
@@ -46,7 +46,7 @@ class PACSConfig(tfds.core.BuilderConfig):
         super(PACSConfig, self).__init__(
             name="{}".format("_".join(self.validation_split)),
             description="pacs dataset",
-            version="0.2.0",
+            version="0.3.0",
             **kwargs)
 
 
