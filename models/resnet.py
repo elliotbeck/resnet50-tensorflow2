9cb053c62532b13e5700f19914832fec97a56235
elliotbeck <elliot.beck@hotmail.com>
2019-12-02 19:28:01 +0100

    fixed typo

--- models/resnet.py
+++ models/resnet.py
@@ -15,16 +15,16 @@ class ResNet50(tf.keras.Model):
                                                     weights= resnet_weights, input_shape=in_shape),
             # tf.keras.layers.Flatten(),
             tf.keras.layers.BatchNormalization(),
-            tf.keras.layers.Dense(512, activation='relu')
-            # tf.keras.layers.Dropout(0.5),
-            # tf.keras.layers.BatchNormalization(),
+            tf.keras.layers.Dense(512, activation='relu'),
+            tf.keras.layers.Dropout(0.5),
+            tf.keras.layers.BatchNormalization(),
             # tf.keras.layers.Dense(128, activation='relu'),
             # tf.keras.layers.Dropout(0.5),
             # tf.keras.layers.BatchNormalization(),
             # tf.keras.layers.Dense(64, activation='relu'),
             # tf.keras.layers.Dropout(0.5),
             # tf.keras.layers.BatchNormalization(),
-            # tf.keras.layers.Dense(num_classes, activation='softmax')
+            tf.keras.layers.Dense(num_classes, activation='softmax')
         ])
         self.model.build([None] + self.input_shape + [3])  # Batch input shape.
 
