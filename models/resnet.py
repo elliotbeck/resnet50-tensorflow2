f8cc7c7788a8d5e08d52d9ae2bc1fd14a3a2b4d6
elliotbeck <elliot.beck@hotmail.com>
2019-12-02 19:57:14 +0100

    removed summary

--- models/resnet.py
+++ models/resnet.py
@@ -29,7 +29,6 @@ class ResNet50(tf.keras.Model):
         self.model.build([None] + self.input_shape + [3])  # Batch input shape.
 
     def call(self, inputs, training=None, mask=None):
-        print(self.model.summary())
         return self.model(inputs, training, mask)
 
     @property
