32560000cb2dd2a464f9345cb4fd61f9909fe503
elliotbeck <elliot.beck@hotmail.com>
2019-12-03 15:29:28 +0100

    changed architecture

--- models/resnet.py
+++ models/resnet.py
@@ -35,7 +35,7 @@ class ResNet50(tf.keras.Model):
         self.model.add(tf.keras.layers.GlobalAveragePooling2D())                                    
         self.model.add(tf.keras.layers.Flatten())
         self.model.add(tf.keras.layers.BatchNormalization())
-        self.model.add(tf.keras.layers.Dense(128, activation='relu'))
+        self.model.add(tf.keras.layers.Dense(256, activation='relu'))
         self.model.add(tf.keras.layers.Dropout(0.5))
         self.model.add(tf.keras.layers.BatchNormalization())
         self.model.add(tf.keras.layers.Dense(128, activation='relu'))
