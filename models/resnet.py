2405bec0237d2dc3763e28517403c6d714c7ac31
elliotbeck <elliot.beck@hotmail.com>
2020-01-24 09:56:03 +0100

    try something new

--- models/resnet.py
+++ models/resnet.py
@@ -9,31 +9,15 @@ class ResNet50(tf.keras.Model):
         self.config = config
 
         in_shape = self.input_shape + [3]
-
-        # self.model = tf.keras.models.Sequential([
-        #     tf.keras.applications.resnet50.ResNet50(include_top=False,
-        #                                             weights= resnet_weights, input_shape=in_shape),                                      
-        #     tf.keras.layers.Flatten(),
-        #     tf.keras.layers.BatchNormalization(),
-        #     tf.keras.layers.Dense(512, activation='relu'),
-        #     tf.keras.layers.Dropout(0.5),
-        #     tf.keras.layers.BatchNormalization(),
-        #     tf.keras.layers.Dense(128, activation='relu'),
-        #     tf.keras.layers.Dropout(0.5),
-        #     tf.keras.layers.BatchNormalization(),
-        #     # tf.keras.layers.Dense(64, activation='relu'),
-        #     # tf.keras.layers.Dropout(0.5),
-        #     # tf.keras.layers.BatchNormalization(),
-        #     tf.keras.layers.Dense(num_classes, activation='softmax')
-        # ])
-
+        
         self.model = tf.keras.models.Sequential()
         self.model.add(tf.keras.applications.resnet50.ResNet50(include_top=False,
                                                     weights= resnet_weights, input_shape=in_shape))
-        # for layer in self.model.layers[:-4]:
-        #     layer.trainable = False 
         self.model.add(tf.keras.layers.GlobalAveragePooling2D())                                    
         self.model.add(tf.keras.layers.Flatten())
+        self.model.add(tf.keras.layers.BatchNormalization())
+        self.model.add(tf.keras.layers.Dense(4096, activation='relu'))
+        self.model.add(tf.keras.layers.Dropout(0.5))
         self.model.add(tf.keras.layers.BatchNormalization())
         self.model.add(tf.keras.layers.Dense(2048, activation='relu'))
         self.model.add(tf.keras.layers.Dropout(0.5))
