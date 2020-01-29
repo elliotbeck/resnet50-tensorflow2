218133cf8b84bfeb1df492802bb5c5207388dfa1
elliotbeck <elliot.beck@hotmail.com>
2020-01-29 08:59:15 +0100

    adjust bash script

--- submit-jobs-2.0.sh
+++ submit-jobs-2.0.sh
@@ -19,17 +19,17 @@ module load cudnn/7.6.4
 export CONFIG="/cluster/home/ebeck/ResNet50/configs/config_class_resnet.json"
 
 export DECAY_EVERY=5000
-export NUM_EPOCHS=20
+export NUM_EPOCHS=40
 export BATCH_SIZE=32
 
 
-for VAR_LEARN_RATE in 0.1 0.01 0.001 0.0001
+for VAR_LEARN_RATE in 0.01 0.001 
 do
     export LEARN_RATE=$VAR_LEARN_RATE
-    for  VAR_DO_RATE in 0.1 0.3 0.5 0.7 0.9 
+    for  VAR_DO_RATE in 0.1 0.3 0.5 0.7 
     do
         export DO_RATE=$VAR_DO_RATE
-        for  VAR_L2_PEN in 0.01 0.001 0.0001 0.00001 
+        for  VAR_L2_PEN in 0.01 0.001 0.0001 0.00001
         do
             export L2_PEN=$VAR_L2_PEN 
             sh submit-train-2.0.sh
