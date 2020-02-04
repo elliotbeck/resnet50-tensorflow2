0651df7699d046cbaf9f8bfa9b8106c9bb056ed5
elliotbeck <elliot.beck@hotmail.com>
2020-02-04 13:56:02 +0100

    performance test

--- main.py
+++ main.py
@@ -71,7 +71,7 @@ def loss_fn(model, features, config, training):
 
     model_output = model(inputs, training=training)
     
-    classification_loss = tf.losses.binary_crossentropy(
+    classification_loss = tf.keras.losses.categorical_crossentropy(
         tf.one_hot(label, axis=-1, depth=config.num_classes),
         model_output, from_logits=False)
     mean_classification_loss = tf.reduce_mean(classification_loss) 
